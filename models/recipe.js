26b738b1a5da595af6eaefc80956f7f3164d1bf2
btb0 <benburke2016@gmail.com>
2023-05-12 21:09:47 -0400

    adds recipe & review schemas

---
+++ models/recipe.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    userName: String,
+    userAvatar: String
+}, {
+    timestamps: true
+});
+
+const recipeSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    ingredients: {
+        type: String,
+        required: true
+    },
+    directions: String,
+    prepTime: {
+        type: Number,
+        min: 0
+    },
+    cookTime: {
+        type: Number,
+        min: 0
+    },
+    reviews: [reviewSchema],
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    userName: String,
+    userAvatar: String
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Recipe', recipeSchema);
--- routes/recipes.js
+++ routes/recipes.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 const ensureLoggedIn = require('../config/ensureLoggedIn');
 
+// All paths start with '/recipes'
+
+// GET /recipes
 
 
 module.exports = router;
