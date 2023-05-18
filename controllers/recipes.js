a4133ccbcc568135a90bde44341708d66cc47f64
btb0 <benburke2016@gmail.com>
2023-05-18 13:27:32 -0400

    fixes img pathing bug

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -30,19 +30,21 @@ async function create(req, res) {
         // Image Upload
         req.body.picture = req.file.path.replace('public/', '');
         // Removes controllers/ from absolute path
-        // const removeCtrl = __dirname.replace('controllers', '');
-        // const removeSpaces = req.body.name.replace(/\s+/g, '');
-        // const oldPath = path.join(removeCtrl, 'public', req.body.picture);
-        // const newPath = path.join(removeCtrl, 'public', removeSpaces)
-        // // Renames image file in /uploads
-        // fs.renameSync(oldPath, newPath, (err) => {
-        //     if (err) {
-        //         console.log('Error renaming image:', err);
-        //     } else {
-        //         console.log('Image renamed successfully!');
-        //     }
-        // });
-        // req.body.picture = 'uploads/' + newPath.split('/')[newPath.split('/').length - 1];
+        const removeCtrl = __dirname.replace('controllers', '');
+        const removeSpaces = req.body.name.replace(/\s+/g, '');
+        const oldPath = path.join(removeCtrl, 'public', req.body.picture);
+        const newPath = path.join(removeCtrl, 'public', 'uploads', removeSpaces)
+        console.log(oldPath)
+        console.log(newPath)
+        // Renames image file in /uploads
+        fs.renameSync(oldPath, newPath, (err) => {
+            if (err) {
+                console.log('Error renaming image:', err);
+            } else {
+                console.log('Image renamed successfully!');
+            }
+        });
+        req.body.picture = 'uploads/' + newPath.split('/')[newPath.split('/').length - 1];
         await Recipe.create(req.body);
         res.redirect('recipes'); // CHANGE TO REDIRECT TO RECIPE DETAILS PAGE ONCE THAT IS ADDED
     } catch (err) {
@@ -60,9 +62,9 @@ async function show(req, res) {
 async function edit(req, res) {
     const recipe = await Recipe.findById(req.params.id);
     // displays edit page with inputs prefilled to current property values
-    res.render('recipes/edit', { recipe });
+    res.render('recipes/edit', { title: 'Edit Recipe', recipe });
 }
-
+//findbyidandupdate
 async function update(req, res) {
     const recipe = await Recipe.findById(req.params.id);
     try {
